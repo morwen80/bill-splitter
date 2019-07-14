07c75c2796a9739ea5ded1696878146b0a3c1cf7
morwen80 <ribesnero80@gmail.com>
2019-07-14 15:27:24 +0200

    refactored Splitting related functions and turned it into one

--- style.js
+++ style.js
@@ -1,39 +1,25 @@
 
 
 const splitting = () => {
-  const total = document.querySelector('#total').value
+  const total = parseInt(document.querySelector('#total').value)
   const people = document.querySelector('#people').value
-  // if PERCENTAGE is present {calcolo}
-  // else if CUSTOM is present {calcolo}
-  //else   const result = total / people
+
 
   if(tipPercent.value.length > 0) {
-    addPercentageToTotal()
-
-  } else if (tipCustom.value.length > 0) {
-    addCustomToTotal()
+    const tipPercent = parseInt(document.querySelector('#tipPercent').value)
+    const totalWithTip = total + (tipPercent * total) / 100;
+    document.querySelector('#perPerson').innerHTML = (totalWithTip / people).toFixed(2);
   }
-
+    else if (tipCustom.value.length > 0) {
+      const tipCustom = parseInt(document.querySelector('#tipCustom').value);
+      document.querySelector('#perPerson').innerHTML = (total + tipCustom / people).toFixed(2);
+  }
   else {
     const result = total / people;
     document.querySelector('#perPerson').innerHTML = result.toFixed(2);
   }
  }
 
-const addPercentageToTotal = () => {
-  const tipPercent = parseInt(document.querySelector('#tipPercent').value)
-  const total = parseInt(document.querySelector('#total').value)
-  const totalWithTip = total + (tipPercent * total) / 100
-  const people = document.querySelector('#people').value
-  document.querySelector('#perPerson').innerHTML = (totalWithTip / people).toFixed(2);
-}
-
-const addCustomToTotal = () => {
-  const tipCustom = parseInt(document.querySelector('#tipCustom').value);
-  const total = parseInt(document.querySelector('#total').value)
-  const people = document.querySelector('#people').value
-  document.querySelector('#perPerson').innerHTML = (total + tipCustom / people).toFixed(2);
-  }
 
 const btn = document.querySelector('#submit')
 btn.addEventListener('click', function(e) {
@@ -50,7 +36,7 @@ btn.addEventListener('click', function(e) {
    if(tipPercent.style.display = "none") {
      tipPercent.style.display = "none";
      tipCustom.style.display = "block";
-     switchBtn.innerHTML = "percentage?"
+     switchBtn.innerHTML = "add a percentage of the total instead"
    }
    // else {
    //   tipCustom.style.display = "none";
